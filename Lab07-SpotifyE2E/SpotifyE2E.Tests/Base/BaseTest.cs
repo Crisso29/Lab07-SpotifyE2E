45de0ce6f7bd10af174c6be4a4ae55dc39b1f8fb
DevCrisso <crisologo.aguilar.27@unsch.edu.pe>
2026-06-29 18:21:42 -0500

    Uploading my README.md

--- Lab07-SpotifyE2E/SpotifyE2E.Tests/Base/BaseTest.cs
+++ Lab07-SpotifyE2E/SpotifyE2E.Tests/Base/BaseTest.cs
@@ -4,12 +4,24 @@ using Microsoft.Playwright.NUnit;
 namespace SpotifyE2E.Tests.Base;
 
 [Parallelizable(ParallelScope.Self)]
-[Retry(2)]  // ← Si el test falla, lo reintenta hasta 2 veces más
 public abstract class BaseTest : PageTest
 {
+    public static readonly string AuthFile =
+        Path.Combine(Directory.GetCurrentDirectory(), "playwright", ".auth", "spotify_state.json");
+
     [OneTimeSetUp]
     public void SetHeaded()
     {
         Environment.SetEnvironmentVariable("HEADED", "1");
+    }
+
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new BrowserNewContextOptions
+        {
+            StorageStatePath = AuthFile,
+            ViewportSize = new ViewportSize { Width = 1280, Height = 720 },
+            Locale = "es-ES"
+        };
     }
 }
